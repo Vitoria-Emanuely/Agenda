439a3cfb0393550db65937dbd8de150065502a54
Vitoria-Emanuely <vikaemanuely@gmail.com>
2017-07-06 14:32:20 -0300

    Alterações

--- controlador_agenda.php
+++ controlador_agenda.php
@@ -1,37 +1,146 @@
 <?php
 
-    function cadastrar(){
+    //
+    function prettyHeader($contatosAuxiliar){
 
-        //controlador agenda
+        $contatosJson = json_encode($contatosAuxiliar, JSON_PRETTY_PRINT); //o array foi codificado para json
 
-        $contatosAuxiliar = file_get_contents('contatos.json');
-        $contatosAuxiliar = json_decode($contatosAuxiliar, true);
-
-        $contato = [
-            'id'       => uniqid(),
-            'nome'     => $_POST['nome'],
-            'email'    => $_POST['email'],
-            'telefone' => $_POST['telefone']
-        ];
-
-        array_push($contatosAuxiliar, $contato);
-
-        $contatosJson = json_encode($contatosAuxiliar, JSON_PRETTY_PRINT);
-
-        file_put_contents('contatos.json', $contatosJson);
+        file_put_contents('contatos.json', $contatosJson); //Está sendpo atribuido strings ao arquivo contatos.json//          HELPPPPPPPPPPPPPPPP
 
         header("Location: index.phtml");
 
+
     }
 
-    function getContact(){
-        $contatosAuxiliar = file_get_contents('contatos.json');
-        $contatosAuxiliar = json_decode($contatosAuxiliar, true);
+
+    //TRANSFORMA  JSON EM ARRAY
+    function codific(){
+
+
+        $contatosAuxiliar = file_get_contents('contatos.json'); //a variavel está recebendo o valor de um arquivo json;
+        $contatosAuxiliar = json_decode($contatosAuxiliar, true); //o arquivo json foi codificado/transformado em um array;
 
         return $contatosAuxiliar;
     }
 
+    //CADASTRA OS CONTATOS
+    function cadastrar($nome,$email,$telefone){
+
+       codific();
+
+
+        $contato = [
+            'id'       => uniqid(),
+            'nome'     => $nome,
+            'email'    => $email,
+            'telefone' => $telefone
+        ];
+
+        array_push($contatosAuxiliar, $contato); //no array $contatosAuxiliar  está sendo adicionado valores do array $contato;
+
+        prettyHeader($contatosAuxiliar);
+
+    }
+
+    //BUSCA OS CONTATOS
+    function buscar($busca = null){
+
+        $contatosAuxiliar= codific();
+
+        $contatosEncontrados = [];
+
+        if ($busca == null || $busca == ""){
+
+            $contatosEncontrados = getContact();
+
+        } else {
+
+            foreach ($contatosAuxiliar as $contato) {
+                if ($contato['nome'] == $busca) {
+                    //echo "Achei o danado";
+                    $contatosEncontrados[] = $contato;
+                }
+
+            }
+        }
+
+        return $contatosEncontrados;
+
+    }
+
+
+
+    //PEGA O CONTATO
+    function getContact(){
+
+       $contatosAuxiliar= codific();
+        return $contatosAuxiliar;
+
+    }
+
+
+    //EXCLUIR O CONTATO
+    function excluirContato($id){
+
+        $contatosAuxiliar= codific();
+
+        foreach($contatosAuxiliar as $posicao => $contato){
+            if($id == $contato['id']){
+
+               unset($contatosAuxiliar[$posicao]);
+            }
+        }
+
+        prettyHeader($contatosAuxiliar);
+
+    }
+
+
+    //EDITAR O CONTATO
+    function editarContato($id){
+
+        $contatosAuxiliar= codific();
+
+        foreach ($contatosAuxiliar as $contato) {
+            if ($contato['id'] == $id) {
+                //echo "Achei o danado";
+                return $contato;
+            }
+
+        }
+
+    }
+
+    //SALVAR CONTATO EDITADO
+    function salvarContatoEditado($id,$nome,$email,$telefone){
+
+        $contatosAuxiliar= codific();
+
+        foreach($contatosAuxiliar as $posicao =>$contato){
+            if($contato['id'] == $id){
+               $contatosAuxiliar[$posicao]['nome']= $nome;
+               $contatosAuxiliar[$posicao]['email']= $email;
+               $contatosAuxiliar[$posicao]['telefone']= $telefone;
+
+               break;
+            }
+        }
+
+        prettyHeader($contatosAuxiliar);
+    }
+
+
     //ROTAS
     if ($_GET['acao'] == 'cadastrar'){
-        cadastrar();
+        cadastrar($_POST['nome'],$_POST['email'], $_POST['telefone'] );
+
+    }elseif($_GET['acao']=='excluir'){
+        excluirContato($_GET['id']);
+
+    }elseif($_GET['acao']=='editar'){
+        salvarContatoEditado($_GET['id'],$_POST['nome'],$_POST['email'], $_POST['telefone'] );
+
+    }elseif($_GET['acao']=='buscar') {
+        buscar($busca);
+
     }
